264c5dea24e2da921439790048b46844796bf4d9
era <the@communist.dev>
2021-11-06 10:15:40 +0000

    Chapter 8

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -34,6 +34,33 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>  {
         return object.toString();
     }
 
+    void executeBlock(List<Stmt> statements,
+                      Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
     @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         Object value = null;
