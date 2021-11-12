c9bddec5b33bb2c832581139b697f2f8addd7ba6
era <the@communist.dev>
2021-11-12 20:50:40 +0000

    Until Chapter 15.2

--- clox/main.c
+++ clox/main.c
@@ -1,22 +1,27 @@
 #include "common.h"
 #include "chunk.h"
+#include "vm.h"
 
 #include "debug.h"
 
 int main(int argc, const char* argv[]) {
-  Chunk chunk;
-  initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+    initVM();
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
 
-  writeChunk(&chunk, OP_RETURN, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
 
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    
+    freeVM();
+    freeChunk(&chunk);
 
-  disassembleChunk(&chunk, "test chunk");
-  freeChunk(&chunk);
-
-  return 0;
+    return 0;
 }
